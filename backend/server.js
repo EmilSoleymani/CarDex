047b1c0ab9c3045e62f75b87d2726ec4ab5be48e
Emil Soleymani <esoleymani@venacorp.com>
2023-11-20 18:15:27 -0500

    Graceful terminate and fetch from S3

--- backend/server.js
+++ backend/server.js
@@ -15,17 +15,40 @@
 const express = require('express');
 const fs = require('fs');
 const cors = require('cors');
+const axios = require('axios')
 
 const app = express();
 const port = 3001;
+const S3_URL = 'https://lecture-slides.s3.amazonaws.com/api.json';
+let apiData;
+let server;
 
 app.use(cors());
 
 // Serve static files from the "public" directory
 app.use(express.static('public'));
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
+/**
+ * Fetches the API data from S3
+ */
+async function fetchApiData() {
+    // Required to prevent "AxiosError: self-signed certificate in certificate chain"
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
+    try {
+      const response = await axios.get(S3_URL);
+      apiData = response.data;
+    } catch (err) {
+      console.error('Error fetching data from S3:', err);
+      // Handle the error (e.g., start with an empty dataset or abort)
+    }
+}
+
+// Fetch data before starting server
+fetchApiData().then(() => {
+    server = app.listen(port, () => {
+      console.log(`Server running at http://localhost:${port}/`);
+    });
 });
 
 // Middleware to parse JSON bodies
@@ -37,19 +60,18 @@ app.post('/api/addSearch', (req, res) => {
     console.log('\nSearch query received:', searchQuery);
     // Handle the search query...
     console.log('Adding search to history...');
-    const data = require('./public/data/api.json');
+    
+    // const data = require('./public/data/api.json');
+    const data = apiData;  // Set data variable to global apiData
     
     // Find all entries in data.data where searchQuery matches entry.make + ' ' + entry.model and add the current datetime to the entry.search_history array
-    data.data.filter(entry => {
+    apiData.data.filter(entry => {
         return entry.year + ' ' + entry.make + ' ' + entry.model === searchQuery;
     }).forEach(entry => {
         entry.search_history.push(new Date().toISOString());
     });
-    
-    // Write the updated data back to the file
-    fs.writeFileSync('./public/data/api.json', JSON.stringify(data, null, 2));
 
-    var updatedData = data.data.filter(entry => {
+    var updatedData = apiData.data.filter(entry => {
       return entry.year + ' ' + entry.make + ' ' + entry.model === searchQuery;
     })
 
@@ -57,33 +79,28 @@ app.post('/api/addSearch', (req, res) => {
 
     // Send a response to the client
     if (updatedData.length > 0) {
-      res.status(201).send('Search history added successfully');
+      res.status(200).send('Search history added successfully');
     } else {
-      res.status(200).send('Nothing updated')
+      res.status(201).send('Nothing updated')
     }
 });
 
 // GET at endpoint /api/trendingModels reads the data at public/data/api.json under entry "data" and sorts by length of search_history field in each entry
 app.get('/api/trendingModels', async (req, res) => {
   try {
-    const data = require('./public/data/api.json');
-
     // For each entry in data.data, delete all entries in search_history that represent a Date().toISOString() older than 30 days
     const thirtyDaysAgo = new Date();  
     thirtyDaysAgo.setDate(thirtyDaysAgo.getDate() - 30);  // Get the date 30 days ago
 
     // Filter out search_history entries older than 30 days
-    data.data.forEach(entry => {
+    apiData.data.forEach(entry => {
         entry.search_history = entry.search_history.filter(dateStr => {
             const searchDate = new Date(dateStr);
             return searchDate > thirtyDaysAgo;
         });
     });
 
-    // Write the updated data back to the file
-    fs.writeFileSync('./public/data/api.json', JSON.stringify(data, null, 2));
-
-    var sortedData = data.data.sort((a, b) => {
+    var sortedData = apiData.data.sort((a, b) => {
       // First sort: by make and model alphabetically
       const makeModelA = a.make.toLowerCase() + ' ' + a.model.toLowerCase();
       const makeModelB = b.make.toLowerCase() + ' ' + b.model.toLowerCase();
@@ -109,10 +126,8 @@ app.get('/api/trendingModels', async (req, res) => {
 // GET at endpoint /api/manufacturers reads the data at public/data/api.json under entry "data" and returns a list of all unique manufacturers
 app.get('/api/manufacturers', async (req, res) => {
   try {
-    const data = require('./public/data/api.json');
-
     // Get a list of all unique manufacturers
-    const manufacturers = [...new Set(data.data.map(entry => entry.make))];
+    const manufacturers = [...new Set(apiData.data.map(entry => entry.make))];
 
     // Sort the list alphabetically
     manufacturers.sort();
@@ -125,3 +140,34 @@ app.get('/api/manufacturers', async (req, res) => {
     res.status(500).send('An error occurred while retrieving the manufacturers');
   }
 });
+
+/**
+ * Gracefully shuts down the server when a termination signal is received
+ * 
+ * @param {*} signal - The termination signal received
+ */
+function gracefulShutdown(signal) {
+    console.log(`${signal} received. Shutting down gracefully.`);
+    
+    server.close(async () => {
+      console.log('Closed out remaining connections.');
+  
+      // Perform your cleanup tasks here
+      // For example, upload updated data to S3
+      // await uploadApiData();
+  
+      console.log('Cleanup completed.');
+  
+      process.exit(0);
+    });
+  
+    // Force close server after a 10s timeout
+    setTimeout(() => {
+      console.error('Could not close connections in time, forcefully shutting down');
+      process.exit(1);
+    }, 10000);  // 10 seconds
+}
+
+// Listen for termination signals and handle them gracefully
+process.on('SIGTERM', () => gracefulShutdown('SIGTERM'));
+process.on('SIGINT', () => gracefulShutdown('SIGINT'));
