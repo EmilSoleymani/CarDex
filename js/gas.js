708ba6d5b9b466abe2b5de9d5a44657e69c23744
Emil Soleymani <esoleymani@venacorp.com>
2023-11-21 16:55:07 -0500

    Fix gasApi

--- js/gas.js
+++ js/gas.js
@@ -15,6 +15,19 @@ $(document).ready(function() {
             longitude = position.coords.longitude;
             
             // API Call to Gas from long and lat
+            $.ajax({
+                url: 'http://localhost:3001/api/gasPrice?lat=' + latitude + '&long=' + longitude,
+                method: 'GET',
+                dataType: 'json',
+                success: function(data) {
+                    $('#estimated-gas-title').html('Estimated Gas Price in ' + data.country);
+                    $('#estimated-gas-amt').html('$' + data.gasoline + ' USD/gal');                    
+                },
+                error: function(error) {
+                    console.error('Error fetching the JSON file:', error);
+                    // Handle any errors here
+                }
+            });
         });
     } else {
         console.log("Geolocation is not supported by this browser.");
